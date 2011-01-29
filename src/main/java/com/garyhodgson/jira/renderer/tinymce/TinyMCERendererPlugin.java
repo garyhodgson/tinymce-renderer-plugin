ebf655a345e38bab1640db7d036b85fef220e1c7
Gary Hodgson <contact@garyhodgson.com>
2011-01-29 21:47:40 +0100

    Issue #7: Add option so that TinyMCE Renderer also translates WikiText

--- src/main/java/com/garyhodgson/jira/renderer/tinymce/TinyMCERendererPlugin.java
+++ src/main/java/com/garyhodgson/jira/renderer/tinymce/TinyMCERendererPlugin.java
@@ -1,15 +1,19 @@
 package com.garyhodgson.jira.renderer.tinymce;
 
+import com.atlassian.jira.config.properties.PropertiesManager;
 import com.atlassian.jira.issue.fields.renderer.IssueRenderContext;
 import com.atlassian.jira.issue.fields.renderer.JiraRendererPlugin;
+import com.atlassian.jira.issue.fields.renderer.wiki.AtlassianWikiRenderer;
 import com.atlassian.jira.plugin.renderer.JiraRendererModuleDescriptor;
 import com.atlassian.jira.util.JiraKeyUtils;
+import com.opensymphony.module.propertyset.PropertySet;
 
 public class TinyMCERendererPlugin implements JiraRendererPlugin {
 
     public static final String TYPE = "tinymce-renderer";
     public static final String LINE_SEPARATOR = System.getProperty("line.separator");
     private JiraRendererModuleDescriptor jiraRendererModuleDescriptor;
+    private AtlassianWikiRenderer atlassianWikiRenderer;
 
     public JiraRendererModuleDescriptor getDescriptor() {
         return jiraRendererModuleDescriptor;
@@ -21,9 +25,19 @@ public class TinyMCERendererPlugin implements JiraRendererPlugin {
 
     public void init(JiraRendererModuleDescriptor jiraRendererModuleDescriptor) {
         this.jiraRendererModuleDescriptor = jiraRendererModuleDescriptor;
+        this.atlassianWikiRenderer = new AtlassianWikiRenderer();
+
     }
 
     public String render(String s, IssueRenderContext issueRenderContext) {
+        PropertySet properties = PropertiesManager.getInstance().getPropertySet();
+        boolean renderWikiText = properties.getBoolean(TinyMCERendererAdminAction.RENDER_WIKI_TEXT_PROPERTY);
+
+        if (renderWikiText) {
+            if (!s.startsWith("<")) {
+                s = atlassianWikiRenderer.render(s, issueRenderContext);
+            }
+        }
         StringBuilder text = new StringBuilder();
         text.append(LINE_SEPARATOR).append(LINE_SEPARATOR);
         text.append(JiraKeyUtils.linkBugKeys(s));
@@ -36,7 +50,17 @@ public class TinyMCERendererPlugin implements JiraRendererPlugin {
     }
 
     public Object transformForEdit(Object obj) {
-        return obj;
+        String s = (String) obj;
+        PropertySet properties = PropertiesManager.getInstance().getPropertySet();
+        boolean renderWikiText = properties.getBoolean(TinyMCERendererAdminAction.RENDER_WIKI_TEXT_PROPERTY);
+
+        if (renderWikiText) {
+            if (!s.startsWith("<")) {
+                s = atlassianWikiRenderer.render(s, null);
+            }
+        }
+
+        return s;
     }
 
     public Object transformFromEdit(Object obj) {
